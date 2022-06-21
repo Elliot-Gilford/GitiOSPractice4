81bf14ad35b2dd153958866f883c223120b2549c
Elliot Gilford <elliotgilford@EGilford-MBP.local>
2022-06-20 20:55:43 -0600

    all files added

---
+++ practice4folderUITests/practice4folderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  practice4folderUITestsLaunchTests.swift
+//  practice4folderUITests
+//
+//  Created by Elliot Gilford on 20/06/2022.
+//
+
+import XCTest
+
+class practice4folderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
